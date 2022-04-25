4981222826e76ff5a36221d5196106ff924eb92b
Bohdan <qwebeck5@gmail.com>
2022-04-25 22:34:47 +0200

    Adds tournament mode

--- network_architecture_optimization/genetic_algorithms/teacher.py
+++ network_architecture_optimization/genetic_algorithms/teacher.py
@@ -5,9 +5,10 @@ from network_architecture_optimization.mappers.network_builder import NetworkBui
 
 class Teacher:
 
-    def __init__(self, X: np.ndarray, y: np.ndarray):
+    def __init__(self, X: np.ndarray, y: np.ndarray, n_evaluations):
         self._input_shape = X[0].shape
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(X, y)
+        self._n_evaluations = n_evaluations
         self.network_builder = NetworkBuilder()
 
     def create_initial_population(self) -> np.ndarray:
@@ -24,7 +25,11 @@ class Teacher:
         return self.network_builder.map_chromosome(self._input_shape, chromosome, data)
 
     def fitness_function(self, solution: np.ndarray, solution_idx: int):
-        model = self.map_chromosome_to_network_architecture(solution)
+        results = [self._evaluate_chromosome(solution) for _ in range(self._n_evaluations)]
+        return np.array(results).mean()
+
+    def _evaluate_chromosome(self, chromosome):
+        model = self.map_chromosome_to_network_architecture(chromosome)
         model.fit(self.x_train, self.y_train, epochs=10)
         _, acc = model.evaluate(self.x_test,  self.y_test, verbose=2)
         return acc
--- network_architecture_optimization/mappers/layer_description.py
+++ network_architecture_optimization/mappers/layer_description.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 class LayerDescription:
     dense_layer_gene: int
     activation_gene: int
-    dropoutRate_gene: int
+    dropout_rate_gene: int
     dropout_gene: int
-    weightInit_gene: int
+    weight_initialization_gene: int
     normalization_gene: int
